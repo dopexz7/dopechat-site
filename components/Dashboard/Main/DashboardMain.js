962469e174b2f094812ec362fce87717d4d23ce4
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-04-16 21:12:37 +0300

    Update DashboardMain.js

--- components/Dashboard/Main/DashboardMain.js
+++ components/Dashboard/Main/DashboardMain.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import DashboardLayout from "./DashboardLayout";
 import DashboardMiddleSection from "./DashboardMiddleSection";
 import YourEmoteSets from "./Rightside/YourEmoteSets";
