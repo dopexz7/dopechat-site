30ddb8946ef60f746fcfb0f7319b9a60b8f169e8
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-07-07 18:32:53 +0300

    now using TypeScript.

--- components/DashboardDark/Main/DashboardMain.tsx
+++ components/DashboardDark/Main/DashboardMain.tsx
@@ -15,7 +15,7 @@ const DashboardMain:FC<DashMainTypes> = ({ data }) =>{
   return (
     <DashboardLayout title="Dashboard">
       <div className="border-[1px] border-white border-opacity-5 shadow-2xl rounded-3xl h-max backdrop-blur-sm max-w-full w-1/5 flex flex-col">
-        <DashboardLeftSignedIn onSuccess={(x : string) => setEditingSet(x)} />
+        <DashboardLeftSignedIn onSuccess={(x : string): any => setEditingSet(x)} />
       </div>
       <div className="shadow-sm backdrop-blur-sm border-[1px] rounded-3xl p-1 border-white border-opacity-5 h-full w-[55%] flex flex-col">
         <DashboardMiddleSection
