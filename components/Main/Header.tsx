29911d519789e43989de8e01e3a9c1a50bf0e560
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-07-15 22:16:26 +0300

    7/15

--- components/Main/Header.tsx
+++ components/Main/Header.tsx
@@ -11,26 +11,26 @@ const Header:FC = () => {
   return (
     <>
       <div className="fixed left-0 w-[100vh] h-[100vw] overflow-scroll overflow-x-hidden -rotate-90 origin-center-50vh">
-        <div className="w-[70vh] h-[70vw] rotate-90">
+        <div className="w-[50vh] h-[70vw] rotate-90">
           <MainHeader />
         </div>
 
-        <div className="w-[70vh] h-[70vw] rotate-90">
+        <div className="w-[50vh] h-[70vw] rotate-90">
           <KeyFeatures />
         </div>
 
-        <div className="w-[70vh] h-[70vw] rotate-90">
+        <div className="w-[50vh] h-[70vw] rotate-90">
           <LookAt />
         </div>
 
-        <div className="w-[70vh] h-[70vw] rotate-90">
+        <div className="w-[50vh] h-[70vw] rotate-90">
           <Changelog />
         </div>
 
-        <div className="w-[70vh] h-[70vw] rotate-90">
+        <div className="w-[50vh] h-[70vw] rotate-90">
           <DonateSection />
         </div>
-        <div className="w-[70vh] h-[70vw] rotate-90">
+        <div className="w-[50vh] h-[70vw] rotate-90">
           <GetStarted />
         </div>
       </div>
