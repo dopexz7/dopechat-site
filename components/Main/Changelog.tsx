3c82efccefcd16a3d948e8821a501871749e5692
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-07-16 17:38:46 +0300

    Update Changelog.tsx

--- components/Main/Changelog.tsx
+++ components/Main/Changelog.tsx
@@ -1,9 +1,22 @@
 import React, { FC } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { leftToRightVariant, rightToLeftVariant } from "./transitionVariants";
-import { isMobile } from "react-device-detect";
 import ChangelogData from "./ChangelogData";
 const Changelog: FC = () => {
+  const [deviceType, setDeviceType] = useState("");
+   useEffect(() => {
+     if (
+       /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone/i.test(
+         navigator.userAgent
+       )
+     ) {
+       setDeviceType("Mobile");
+     } else {
+       setDeviceType("Desktop");
+     }
+   }, []);
+
   return (
     <>
       <div
@@ -11,7 +24,7 @@ const Changelog: FC = () => {
         id="section3"
       >
         <div className="mr-auto w-screen max-w-6xl h-3/4 space-x-6 lg:space-x-20 p-6 flex flex-row lg:flex-row justify-center items-center">
-          {isMobile ? (
+          {deviceType === "Mobile" ? (
             <div className="text-2xl lg:text-8xl font-normal lg:font-light tracking-tight">
               Changelog
             </div>
@@ -26,7 +39,7 @@ const Changelog: FC = () => {
               Changelog
             </motion.div>
           )}
-          {isMobile ? (
+          {deviceType === "Mobile" ? (
             <div className="h-5/6 p-3 font-normal rounded-xl flex flex-col scrollbar-thin scrollbar-thumb-white scrollbar-track-main-white overflow-y-scroll w-full text-sm border-[1px] border-white border-opacity-5 shadow-2xl ">
               <ChangelogData />
             </div>
