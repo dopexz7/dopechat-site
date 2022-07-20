94d4bac48552bac177abf79bf24a64b5262accb4
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-07-20 17:06:30 +0300

    Update Changelog.tsx

--- components/Main/Changelog.tsx
+++ components/Main/Changelog.tsx
@@ -1,11 +1,7 @@
 import React, { FC } from "react";
 import { changelogData } from "./changelogData";
-import * as Cg from "react-icons/cg";
-import * as Md from "react-icons/md";
-import { useGetDevType } from "./useGetDevType";
 
 const Changelog: FC = () => {
-  const deviceType = useGetDevType();
   return (
     <>
       <div
@@ -20,20 +16,6 @@ const Changelog: FC = () => {
             changelog
           </div>
           <div className="h-5/6 ml-3 p-3 font-normal rounded-xl flex flex-col overflow-y-scroll w-full text-sm border-[1px] border-white border-opacity-5 shadow-2xl bg-black bg-opacity-10">
-            <div className="flex flex-row items-center relative">
-              <div className="flex items-center opacity-50 text-md top-1 absolute right-3">
-                {deviceType === "Mobile" ? (
-                  <>
-                    <Md.MdOutlineSwipe className="mr-1" />
-                    SWIPE DOWN
-                  </>
-                ) : (
-                  <>
-                    <Cg.CgMouse className="mr-1" /> SCROLL DOWN
-                  </>
-                )}
-              </div>
-            </div>
             {Object.keys(changelogData).map((key, index) => (
               <div key={index}>
                 <div className="bg-darker-purple px-3 py-1 flex text-main-white w-max rounded-2xl">
